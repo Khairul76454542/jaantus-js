1a9d03f29460a86c159335ae88ba56d1383b27ac
khairul76454542 <khairul76454542@gmail.com>
2023-08-02 00:31:24 +0600

    same js practice

---
+++ operation.js
@@ -0,0 +1,22 @@
+var onionprice = 20;
+var eggprice = 10;
+
+// var totalprice = onionprice + eggprice;
+
+// var priceDifference = onionprice - eggprice;
+// console.log(priceDifference);
+// console.log(totalprice);
+// console.log(onionprice);
+// console.log(eggprice);
+// console.log(onionprice + eggprice);
+
+var orangePrice = 20;
+var quantiy = 7;
+var totalCost = orangePrice * quantiy;
+// console.log(totalCost);
+
+// division
+var money = 500;
+var player = 10;
+var eachPlayer = money / player;
+console.log(eachPlayer);
